93daa86d6503b6e17188c167a68261fbdd709e1a
vingojw@gmail.com <vingojw@gmail.com>
2016-05-13 18:31:07 +0800

    u

--- src/app.js
+++ src/app.js
@@ -7,6 +7,11 @@ var router = require('koa-router')();
 var logger = require('koa-logger');
 app.use(logger());
 
+//上传
+var multer = require('koa-multer');
+var uploads = multer({dest:'uploads/'});
+var uploads1 = multer({dest:'uploads1/'});
+
 //router post请求  https://github.com/koajs/bodyparser/tree/3.x
 var bodyParser = require('koa-bodyparser');
 app.use(bodyParser());
@@ -28,12 +33,23 @@ router
 		// Cookie.prototype.httpOnly = true;
 		// Cookie.prototype.secure = false;
 		// Cookie.prototype.overwrite = false;
-		ctx.body = '121932631112635269';
+		ctx.body = ` 
+		<form action="/profile" method="POST" enctype="multipart/form-data">
+		  单文件:
+		  <input type="file" name="avatar">
+		  <input type="submit" value="Upload Image">
+		</form>
+		<form action="/profilelist" method="POST" enctype="multipart/form-data">
+		  多文件:
+		  <input type="file" name="avatar">
+		  <input type="file" name="gallery">
+		  <input type="submit" value="Upload Image">
+		</form>`;
 	})
 	.get('/users/:id', function (ctx, next){
 		//http://localhost:3000/users/123
 		ctx.body = {search:ctx.params.id};
-	})
+	})  
 	.post('/post', function(ctx, next){
 		// //浏览器发起post请求
 		// xhr = new XMLHttpRequest();
@@ -60,13 +76,35 @@ router
 	get('/close', function(ctx){
 		return ctx.req.destroy();
 	})
-
+	.post('/profile', uploads.single('avatar'),function(ctx,next){
+		 debugger;
+		 console.log(ctx);
+		 //ctx.req.file 看起来是下面这样
+		 // {
+		 //   "fieldname": "avatar",
+		 //   "originalname": "a.js",
+		 //   "encoding": "7bit",
+		 //   "mimetype": "application/javascript",
+		 //   "destination": "uploads/",
+		 //   "filename": "701f8f88c310f34401c2f74f337794b0",
+		 //   "path": "uploads\\701f8f88c310f34401c2f74f337794b0",
+		 //   "size": 6189
+		 // }
+	})
+	//多文件
+	.post('/profilelist',multer({dest:'uploads1/'}).fields([
+		{ name: 'avatar', maxCount: 1 },
+		{ name: 'gallery', maxCount: 8 }
+	]), function(ctx){
+		ctx.body="oooook";
+	} )
+ 
 	function timeOut(ms){
 		return new Promise((resolve)=>{
 			setTimeout(function(){
 				resolve('ok');
 			},ms);
-		})
+		});
 	}
 
 
@@ -103,4 +141,5 @@ app
 	.use(router.allowedMethods())
 	.listen(3000);
 
-console.log('http://localhost:3000');
+console.log('访问：http://localhost:3000');
+console.log('可以编辑./src/app.js，保存后自动重启服务');
